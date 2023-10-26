7738ca6d5223580fab5dad4f0be52a7d26fde04f
University of Arizona Sensor Lab <134433408+UASensorLab@users.noreply.github.com>
2023-10-26 12:03:34 -0700

    Add files via upload

---
+++ bleak_receiver_corsense.py
@@ -0,0 +1,72 @@
+import asyncio
+import signal
+from bleak import BleakClient, BleakScanner
+
+YOUR_DEVICE_NAME = "CorSense B-05384"
+CHARACTERISTIC_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
+
+def callback(sender: int, data: bytearray):
+    index = 1
+    measurement = list(data)
+
+    if measurement[0] & 0x01 == 0x01:
+        print(f"Heart Rate: {int.from_bytes(measurement[index:index+2], byteorder='little')} bpm")
+        index += 2
+    else:
+        print(f"Heart Rate: {measurement[index]} bpm")
+        index += 1
+
+    if measurement[0] & 0x04 == 0x04:
+        if measurement[0] & 0x02 == 0x02:
+            print("Sensor contact detected")
+        else:
+            print("No or poor sensor contact detected")
+
+    if measurement[0] & 0x08 == 0x08:
+        print(f"Energy expended: {int.from_bytes(measurement[index:index+2], byteorder='little')} kilojoules")
+        index += 2
+
+    if measurement[0] & 0x10 == 0x10:
+        for i in range(index, len(measurement), 2):
+            print(f"RR interval {i-index+1}: {int.from_bytes(measurement[i:i+2], byteorder='little')/1024} seconds")
+    
+    print("\n")
+
+async def main():
+    devices = await BleakScanner.discover()
+    target_device = None
+
+    for device in devices:
+        if device.name == YOUR_DEVICE_NAME:
+            print("Found target device: ", device.name)
+            target_device = device
+            break
+
+    if not target_device:
+        print(f"Device {YOUR_DEVICE_NAME} not found!")
+        return
+    
+    async with BleakClient(target_device.address, timeout=60) as client:
+        print("Device connected. Starting notifications...")
+
+        await client.start_notify(CHARACTERISTIC_UUID, callback)
+
+        stop_event = asyncio.Event()
+
+        def signal_handler(sig, frame):
+            stop_event.set()
+
+        signal.signal(signal.SIGINT, signal_handler)
+
+        try:
+            while not stop_event.is_set():
+                await asyncio.sleep(0)
+        except asyncio.CancelledError or Exception:
+            pass
+
+        await client.stop_notify(CHARACTERISTIC_UUID)
+        print("Stopped notifications.")
+        return
+
+if __name__ == "__main__":
+    asyncio.run(main())
